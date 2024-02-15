fb1d806675fe60d3c163732287c44ed007f08724
matteocicigoi <matteo.cicigoi1@hotmail.it>
2024-02-15 15:05:06 +0100

    fix: route

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 */
 
-Route::get('/', [ProjectsController::class, 'index']);
+Route::get('/projects', [ProjectsController::class, 'index']);
