d26bd9d2e3803f9d74922b2c0afa6ddf65c28b08
matteocicigoi <matteo.cicigoi1@hotmail.it>
2024-02-15 18:33:38 +0100

    feat: 8

--- app/Http/Controllers/Api/ProjectsController.php
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProjectsController extends Controller
 {
     public function index() {
-        $projects = Project::with('type', 'technologies')->paginate(12);
+        $projects = Project::with('type', 'technologies')->paginate(8);
         return response()->json([
                 'success' => true,
                 'results' => $projects
